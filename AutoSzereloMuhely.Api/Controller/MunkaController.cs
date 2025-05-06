9782b23e282a8cfd9cb5477754ff1a7dc6dec1b3
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-05-06 15:06:24 +0200

    feat: MunkaController.cs and UgyfelController.cs implemeted

--- AutoSzereloMuhely.Api/Controller/MunkaController.cs
+++ AutoSzereloMuhely.Api/Controller/MunkaController.cs
@@ -18,14 +18,51 @@ public class MunkaController : ControllerBase
     [HttpPost]
     public IActionResult Add([FromBody] Munka munka)
     {
-        var exists = _munkaService.Get(munka.MunkaID);
+        _munkaService.Add(munka);
+        return Ok();
+    }
 
-        if (exists is not null)
+    [HttpGet]
+    public ActionResult<List<Munka>> GetAll()
+    {
+        var munkak = _munkaService.GetAll();
+        return Ok(munkak);
+    }
+
+    [HttpGet("id")]
+    public ActionResult<Munka> Get(int id)
+    {
+        var munka = _munkaService.Get(id);
+        return munka;
+    }
+
+    [HttpDelete("id")]
+    public ActionResult Delete(int id)
+    {
+        var munka = _munkaService.Get(id);
+        if (munka is not null)
         {
-            return Conflict();
+            _munkaService.Delete(munka.MunkaID);    
         }
 
-        _munkaService.Add(munka);
+        return Ok();
+    }
+
+    [HttpPut("id")]
+    public IActionResult Update(int id, [FromBody] Munka munka)
+    {
+        if (id != munka.MunkaID)
+        {
+            return BadRequest();
+        }
+
+        var oldMunka = _munkaService.Get(munka.MunkaID);
+
+        if (oldMunka is null)
+        {
+            return NotFound();
+        }
+        _munkaService.Update(munka);
         return Ok();
     }
     
--- AutoSzereloMuhely.Api/Controller/UgyfelController.cs
+++ AutoSzereloMuhely.Api/Controller/UgyfelController.cs
@@ -10,4 +10,42 @@ public class UgyfelController : ControllerBase
 {
     private readonly IUgyfelService _ugyfelService;
     
+    public UgyfelController(IUgyfelService ugyfelService)
+    {
+        _ugyfelService = ugyfelService;
+    }
+    
+    [HttpPost]
+    public IActionResult Add([FromBody] Ugyfel ugyfel)
+    {
+        _ugyfelService.Add(ugyfel);
+        return Ok();
+    }
+    
+    [HttpGet]
+    public ActionResult<List<Ugyfel>> GetAll()
+    {
+        var ugyfelek = _ugyfelService.GetAll();
+        return Ok(ugyfelek);
+    }
+    
+    [HttpGet("id")]
+    public ActionResult<Ugyfel> Get(int id)
+    {
+        var ugyfel = _ugyfelService.Get(id);
+        return ugyfel;
+    }
+    
+    [HttpDelete("id")]
+    public ActionResult Delete(int id)
+    {
+        var ugyfel = _ugyfelService.Get(id);
+        if (ugyfel is not null)
+        {
+            _ugyfelService.Delete(ugyfel.UgyfelId);    
+        }
+
+        return Ok();
+    }
+    
 }
